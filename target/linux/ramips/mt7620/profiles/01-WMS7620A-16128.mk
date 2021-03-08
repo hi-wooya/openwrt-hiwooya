207c6ce34b1974b4683d113c1b9c5dd0fe59afa1
wooya <wooya@hi-wooya.com>
2021-03-08 11:15:41 +0800

    mt7620 sd

--- target/linux/ramips/mt7620/profiles/01-WMS7620A-16128.mk
+++ target/linux/ramips/mt7620/profiles/01-WMS7620A-16128.mk
@@ -9,7 +9,17 @@ define Profile/WMS7620A-16128
 	NAME:=WMS7620A-16128
 	PACKAGES:=\
 		kmod-usb-core kmod-usb2 kmod-usb-ohci \
-		kmod-ledtrig-usbdev
+		kmod-ledtrig-usbdev \
+                mountd \
+                mjpg-streamer \
+                uhttpd rpcd rpcd-mod-iwinfo \
+                rpcd-mod-rpcsys cgi-io spi-tools \
+                kmod-fs-vfat kmod-fs-exfat kmod-fs-ext4 block-mount e2fsprogs \
+                kmod-i2c-core kmod-i2c-mt7621 \
+                kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 \
+                kmod-sdhci-mt7620 kmod-usb-storage \
+                kmod-video-core kmod-video-uvc \
+                ralink-wifi luci
 endef
 
 define Profile/WMS7620A-16128/Description
